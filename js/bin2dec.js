10eaedfdb948ce867cd04c5eeae2e9959ed3ee03
luan <aluncard6@gmail.com>
2021-01-11 20:42:21 -0300

    verificacao de digitos ver:1

--- js/bin2dec.js
+++ js/bin2dec.js
@@ -1,10 +1,45 @@
 
 var verificacao
 var entrada
+var x=0
+var numero=''
+ function recupera (){
+     
+     
+     
 
-var recupera = function(){
+    entrada=document.getElementById('entrada').value
     
+
+    x=entrada.length -1
+    
+
+    
+
+
+    if(entrada.charAt(x)!='1' && entrada.charAt(x)!='0'){
+        console.log('entre apenas com 0 e 1')
+        var n = entrada.replace(entrada.charAt(x),'')
+        document.getElementById('entrada').value=n
+        
+    }else if(x==8){
+        var n = entrada.replace(entrada.charAt(x),'')
+        document.getElementById('entrada').value=n
+        console.log('maximo de 8 números !!!')
+    }
+    
+    
+
+    
+       
+
+
+   
+
+
 }
+
+
 
 
 
