a91407d458065dcb596fbc0c9ecbe8d878a2e381
luan <aluncard6@gmail.com>
2021-01-14 16:39:27 -0300

    alerando js

--- js/bin2dec.js
+++ js/bin2dec.js
@@ -1,33 +1,15 @@
 
-var verificacao
-var entrada
-var x=0
-var numero=''
- function recupera (){
-     
-     
-     
-
-    entrada=document.getElementById('entrada').value
-    
-
-    x=entrada.length -1
-    
-
-    
-
-
-    if(entrada.charAt(x)!='1' && entrada.charAt(x)!='0'){
-        console.log('entre apenas com 0 e 1')
-        var n = entrada.replace(entrada.charAt(x),'')
-        document.getElementById('entrada').value=n
-        
-    }else if(x==8){
-        var n = entrada.replace(entrada.charAt(x),'')
-        document.getElementById('entrada').value=n
-        console.log('maximo de 8 números !!!')
+function converter (){
+    const bin =document.getElementById('entrada')
+    if (bin===''){
+        return alert("insira um número binário")
     }
+
     
+
+
+
+}
     
 
     
@@ -37,7 +19,6 @@ var numero=''
    
 
 
-}
 
 
 
